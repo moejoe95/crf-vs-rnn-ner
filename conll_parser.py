fd354ded76affb334fcadba8682eb8b720438ae0
moejoe95 <j.gugglberger@gmail.com>
2019-11-18 16:13:50 +0100

    update conll_parser, to support CoNLL 2003 dataset

--- conll_parser.py
+++ conll_parser.py
@@ -1,14 +1,15 @@
 # parser for conll files
+import re
 
 def parse(filename):
     with open(filename, "r") as f:
         docs = []
         doc = []
         for line in f:
-            word_label = line.split("\t")
+            word_label = re.split(' |\t', line)
             if len(word_label) < 2 or len(word_label[0].strip()) == 0:
                 docs.append(doc)
                 doc = []
                 continue
-            doc.append((word_label[0].strip(), word_label[1].strip()))
+            doc.append((word_label[0].strip(), word_label[len(word_label)-1].strip()))
     return docs
