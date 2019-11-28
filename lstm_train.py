6cd8e6484c61affc9141c5fc07b23e6de5ca00da
moejoe95 <j.gugglberger@gmail.com>
2019-11-28 16:07:53 +0100

    udpate lstm approach

--- lstm_train.py
+++ lstm_train.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-"""lstm_train, a tool to train the CNN NER system with a given conll file.
+"""lstm_train, a tool to train and/or test the CNN NER system with a given conll file.
 
 Usage:
-  lstm_train.py [(-m | --model) <model>] [(-t | --train) <file>]
+  lstm_train.py [(-t | --train) <file>]
   lstm_train.py (-h | --help)
 
 Options:
@@ -11,63 +11,83 @@ Options:
 from docopt import docopt
 import conll_parser
 import numpy as np
-from keras.models import Sequential, Model, Input
+from keras.models import Model, Input, load_model
 from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional, Activation
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-from keras.callbacks import ModelCheckpoint
-from keras.optimizers import Adam
+from sklearn_crfsuite.metrics import flat_classification_report
+from sklearn.model_selection import train_test_split
+
 
 arguments = docopt(__doc__, version='lstm_train')
 
 test_file = arguments.get('<file>')
-test_file = './data/conll/eng.train' if test_file == None else test_file
-model_name = arguments.get('<model>')
-model_name = 'cnn.h5' if model_name == None else model_name
+if test_file == None:
+  test_file = './data/conll/eng.all'
+  print('no file specified, use default:', test_file, '...')
+
 
 # parse file
-docs, sentences, labels = conll_parser.parse(test_file)
+docs, sentences = conll_parser.parse(test_file)
 
 # flatten 2D lists
 words = [w for sen in sentences for w in sen]
-labels = [l for sen in labels for l in sen]
+words.append('-PAD-')
 
 # unique
 words = list(set(words))
-labels = list(set(labels))
 
 # Dictionary word:index 
 word2idx = {w : i for i, w in enumerate(words)}
-labels2idx = {'I-LOC': 0, 'I-PER': 1, 'B-LOC': 2, 'O': 3, 'I-ORG': 4, 'B-MISC': 5, 'B-ORG': 6, 'I-MISC': 7}
+
+labels2idx = labels = {
+    'I-LOC':    0, 
+    'B-LOC':    1, 
+    'I-PER':    2, 
+    'B-PER':    3,
+    'I-ORG':    4,
+    'B-ORG':    5, 
+    'I-MISC':   6,
+    'B-MISC':   7, 
+    'O':        8,
+    '-PAD-':    9
+}
+idx2label = {i: w for w, i in labels2idx.items()}
 
 max_len = 100 # hard coded max length of sentence
 
 # words
 X = [[word2idx[w[0]] for w in s] for s in docs]
-X = pad_sequences(maxlen = max_len, sequences = X, padding = "post", value = 0)
+X = pad_sequences(maxlen = max_len, sequences = X, padding = "post", value = word2idx['-PAD-'])
 
 # labels
 y = [[labels2idx[w[1]] for w in s] for s in docs]
-y = pad_sequences(maxlen = max_len, sequences = y, padding = "post", value = 0)
-y = [to_categorical(i, num_classes = len(labels)) for i in y]
+y = pad_sequences(maxlen = max_len, sequences = y, padding = "post", value = labels2idx['-PAD-'])
+y = [to_categorical(i, num_classes = len(labels2idx)) for i in y]
+
+# split data
+X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.15)
 
 # Model architecture
+input = Input(shape=(max_len,))
+model = Embedding(input_dim=len(words), output_dim=50, input_length=max_len)(input)
+model = Dropout(0.1)(model)
+model = Bidirectional(LSTM(units=100, return_sequences=True, recurrent_dropout=0.1))(model)
+out = TimeDistributed(Dense(len(labels2idx), activation="softmax"))(model)  # softmax output layer
+model = Model(input, out)
 
-hidden_size = 500
-model = Sequential()
-model.add(Embedding(len(words), hidden_size, input_length=max_len))
-model.add(LSTM(hidden_size, return_sequences=True))
-model.add(LSTM(hidden_size, return_sequences=True))
-model.add(Dropout(0.5))
-model.add(TimeDistributed(Dense(len(labels))))
-model.add(Activation('softmax'))
+# compile and fit model
+model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
+model.fit(X_tr, np.array(y_tr), batch_size=32, epochs=5, validation_split=0.1, verbose=1)
 
+# Evaluation
+y_pred = model.predict(X_te)
+y_pred = np.argmax(y_pred, axis=-1)
+y_test_true = np.argmax(y_te, axis=-1)
 
-optimizer = Adam()
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
+y_pred = [[idx2label[i] for i in row] for row in y_pred]
+y_test_true = [[idx2label[i] for i in row] for row in y_test_true]
 
-checkpointer = ModelCheckpoint(filepath = model_name, verbose = 1, mode = 'auto', save_best_only = True, monitor='val_loss')
-
-model.fit(X, np.array(y), batch_size=32, epochs=3, callbacks=[checkpointer])
-
-model.save(model_name)
+# print report
+report = flat_classification_report(y_pred=y_pred, y_true=y_test_true)
+print(report)
